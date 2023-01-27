a4ffd65b94a9c5254bd102b734c8d910cab03ad8
Jettyjaaaaa <123004162+WishSemangern@users.noreply.github.com>
2023-01-27 10:13:46 +0000

    check2

--- .ccls-cache/@home@runner@bubble-sort-WishSemangern/main.c
+++ .ccls-cache/@home@runner@bubble-sort-WishSemangern/main.c
@@ -13,23 +13,27 @@ int Isprime(int num){
 }
 
 int main(int argc,char *argv[]) {
-  int i,j,*a,n;
+  int i,j=0,*a,n,*prime;
   n = argc-1;
   a=(int*)malloc(sizeof(int)*n);
-  int *prime=(int*)malloc(sizeof(int)*n);
+  prime = (int*)malloc(sizeof(int)*n);
   for(i=0;i<n;i++){
     a[i] = atoi(argv[i+1]);
-    if(Isprime(a[0])){
+    if(Isprime(a[i])){
       prime[j]=a[i];
       j++;
     }
-    n=j;
-    if(j==0) printf("No prime\n");
-    else{
-      display(prime,n);
-      bubbleSort(prime,n);
     }
-    display(prime,n);
+    
+if(j==0){
+  printf("No prime\n");
+      }
+else{
+  n=j;
+  display(prime,n);
+  bubbleSort(prime,n);
+  
+  display(prime,n);
   }
    return 0;
 }
